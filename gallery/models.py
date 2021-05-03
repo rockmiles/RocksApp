c203d558304f615cee4ec550c990af80a56e347b
Okoth Pius <orukopius8@gmail.com>
2021-05-03 19:49:47 +0300

    feat: update

--- gallery/models.py
+++ gallery/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 from PIL import Image
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    images = models.ImageField(upload_to='images/')
+    date_posted = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(upload_to='images/')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
