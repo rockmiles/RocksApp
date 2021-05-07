f13781c99f402d1094d8face917817d292d0af7f
Okoth Pius <orukopius8@gmail.com>
2021-05-07 20:29:49 +0300

    feat: update

--- gallery/views.py
+++ gallery/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'gallery/home.html')
 
-def postdetail(request):
+def posts(request):
     posts = Post.objects.all()
     return render(request, 'gallery/post-detail.html', {'posts' : posts})
