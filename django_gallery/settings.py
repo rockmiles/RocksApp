8034b0900aac073b4adc0bceafe48a8ec68fc52e
Okoth Pius <orukopius8@gmail.com>
2021-05-07 20:51:23 +0300

    feat: update

--- django_gallery/settings.py
+++ django_gallery/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'gallery.apps.GalleryConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
