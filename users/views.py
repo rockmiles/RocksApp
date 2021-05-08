c99a8f0379f489a67c2d16888b10d1b55c17bb72
Okoth Pius <orukopius8@gmail.com>
2021-05-08 17:46:45 +0300

    fix: update

--- users/views.py
+++ users/views.py
@@ -10,11 +10,11 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username} Log In to Proceed!')
-            return redirect('posts')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     context = {'form':form}
-    return render(request, 'users/register.html')
+    return render(request, 'users/register.html', context)
 
 
 @login_required
